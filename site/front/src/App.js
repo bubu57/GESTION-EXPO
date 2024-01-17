bf2f6eee008d0d3c2e657fd24f9ba55b184cb0b7
bubu57 <bunelierpro@gmail.com>
2024-01-17 11:42:23 +0100

    / print of exposition info

--- site/front/src/App.js
+++ site/front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,6 +12,24 @@ const App = () => {
     setMsg(msg)
   }
 
+  const [expositions, setExpositions] = useState([]);
+  const [lieu, setLieu] = useState([]);
+
+  useEffect(() => {
+    // Appel à l'API du backend lors du montage du composant
+    fetch('/api/app')
+      .then(response => response.json())
+      .then(data => setExpositions(data))
+      .then(data => setLieu(data))
+      .catch(error => console.error('Erreur:', error));
+  }, []);
+
+
+
+  const formatDateString = (dateString) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
   return (
     <div className="App">
@@ -22,6 +40,11 @@ const App = () => {
         </p>
         <button onClick={HandleClick}>Bonjour</button>
         <p>{msg}</p>
+        <ul>
+          {expositions.map(exposition => (
+            <li key={exposition.id}>Nom: {exposition.nom}   Date debut:{formatDateString(exposition.date_debut)}   Date fin:{formatDateString(exposition.date_fin)}   Ville:{exposition.ville}   Places:{exposition.quota}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
--- site/server.js
+++ site/server.js
@@ -1,19 +1,52 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');  // Ajout du module mysql2
 require('dotenv').config()
 const app = express();
 
 // recuperation du port via .env sinon utilise le port 5000
 const PORT = process.env.PORT || 5000;
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Erreur de connexion à la base de données:', err);
+    } else {
+      console.log('Connexion à la base de données réussie');
+    }
+});
+
 app.use(express.json())
 app.use(express.static('front/build'))
 
 app.get('/api/app', (req, res) => {
-    res.send({
-        msg: 'Hello world'
-    })
-})
+    const requette = `
+      SELECT Exposition.*, Lieu.ville
+      FROM Exposition
+      JOIN Lieu ON Exposition.id_lieu = Lieu.id
+    `;
+    connection.query(requette, (err, results) => {
+      if (err) {
+        console.error('Erreur lors de la récupération des données de la table exposition:', err);
+        res.status(500).json({ error: 'Erreur interne du serveur' });
+      } else {
+        res.json(results)
+      }
+    });
+});
+
+
+// app.get('/api/app', (req, res) => {
+//     res.send({
+//         msg: 'Hello world'
+//     })
+// })
 
 app.get('/*', (_, res) => {
     res.sendFile(path.join(__direname, '/front/build/index.html'));
