a117a81be91afb3de7caf99b07857653af09ed27
bubu57 <bunelierpro@gmail.com>
2024-01-21 11:56:57 +0100

    up

--- site/front/src/pages/enregistrement_expo.js
+++ site/front/src/pages/enregistrement_expo.js
@@ -38,8 +38,95 @@ const EnregistrementExpo = () => {
         console.error('Erreur lors de la requête:', error);
       });
   };
-
-  return (<div dangerouslySetInnerHTML={{ __html: htmlContent }} />);
+  return(
+    <div className='container'>
+      <div className='img'></div>
+      <div className='form'>
+        <center><p className='title'>Enregistrement exposition</p></center>
+        <form onSubmit={handleSubmit}>
+          <div className='form-block'>
+            <div className='form-name'>
+              <p className='label'>Nom exposition</p>
+              <p className='label'>Date debut</p>
+              <p className='label'>Date fin</p>
+              <p className='label'>Quota</p>
+              <p className='label'>Genre</p>
+              <p className='label'>Adresse</p>
+              <p className='label'>Attitude</p>
+              <p className='label'>Longitude</p>
+            </div>
+            <div className='form-input'>
+              <div className='div-input'>
+                <input
+                  className='nom'
+                  type="text"
+                  placeholder="Paris exposition"
+                  name="nom"
+                  value={formData.nom}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='div-input'>
+                <input
+                  className='date_debut'
+                  type="text"
+                  placeholder="21/02/2024"
+                  name="date_debut"
+                  value={formData.date_debut}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='div-input'>
+                <input
+                  className='date_fin'
+                  type="text"
+                  placeholder="22/02/2024"
+                  name="date_fin"
+                  value={formData.date_fin}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='div-input'>
+                <input
+                  className='quota'
+                  type="text"
+                  placeholder="200"
+                  name="quota"
+                  value={formData.quota}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='div-input'>
+                <input
+                  className='type'
+                  type="text"
+                  placeholder="Art"
+                  name="type"
+                  value={formData.type}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='div-input'>
+                <input
+                  className='lieu'
+                  type="text"
+                  placeholder="13 bis rue de paris 54000 paris"
+                  name="lieu"
+                  value={formData.lieu}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <center>
+            <div className='button'>
+              <button type="submit" className='button-text'>Enregistrer</button>
+            </div>
+          </center>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default EnregistrementExpo;
