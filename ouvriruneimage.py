668c6f3291a8462883fce8da7eb096e6a28408f5
Adrien57 <122373319+Adrien57@users.noreply.github.com>
2024-02-05 09:28:24 +0100

    Add files via upload

---
+++ ouvriruneimage.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+file_path = '/home/adrienclement/TEST/image.jpg'
+
+image = Image.open(file_path)
+
+image.show()
---
+++ programmetestQRCODE.py
@@ -0,0 +1,25 @@
+import zbarlight
+from PIL import Image
+
+def read_qr_code(image_path):
+    with open(image_path, 'rb') as image_file:
+        image =Image.open(image_file)
+        image.load()
+
+    codes = zbarlight.scan_codes(['qrcode'], image)
+
+    if codes is not None and len(codes) > 0:
+        qr_code_data = codes[0].decode('utf-8')
+        return qr_code_data
+    else: 
+        return None
+
+if __name__ == '__main__':
+    image_path = '/home/adrienclement/TEST/image.jpg'
+    qr_code_data = read_qr_code(image_path)
+
+
+    if qr_code_data:
+        print("données du QR code :", qr_code_data)
+    else:
+        print('aucun qr code trouvé dans l image')
