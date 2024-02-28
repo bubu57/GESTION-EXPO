1c62880e93c8fed993f002dc69504113344e9ab2
bubu57 <bunelierpro@gmail.com>
2024-02-28 07:22:41 +0100

    clean v3

--- site/front/src/index.js
+++ site/front/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/register_expo.css";
 import "./styles/liste_expo.css";
-import "./styles/qrcode.css"
 import "./styles/register_user.css"
-import "./styles/register-form-visitor.css"
 import "./styles/header.css"
 import "./styles/testemail.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
