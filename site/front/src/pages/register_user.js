c0a1324b31fb52cbaf06a6fcdf5407562debcdb4
bubu57 <bunelierpro@gmail.com>
2024-03-15 10:03:16 +0100

    not work

--- site/front/src/pages/register_user.js
+++ site/front/src/pages/register_user.js
@@ -27,13 +27,6 @@ const FormEnregistrements = () => {
       });
   }, []);
 
-  const handleExpoChange = (e) => {
-    const selectedExpoId = e.target.value;
-    setFormData({
-      ...formData,
-      id_expo: selectedExpoId,
-    });
-  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -43,77 +36,32 @@ const FormEnregistrements = () => {
     });
   };
 
-  const handleDateChange = (date) => {
-    setFormData({
-      ...formData,
-      date_debut: date,
-    });
-  };
-
-  const generateQRCode = async () => {
-    try {
-      const qrCodeData = `${formData.prenom} ${formData.nom} ${dayjs(formData.date_debut).format('DD/MM/YYYY')} ${formData.heure} ${formData.id_expo}`;
-      const qrCodeDataURL = await QRCode.toDataURL(qrCodeData);
-      return qrCodeDataURL;
-    } catch (error) {
-      console.error('Erreur lors de la génération du QR code:', error);
-      throw error;
+  const handleExpoChange = (e) => {
+    let selectedExpoId = e.target.value;
+    let selectedExpo;
+    for (let i = 0; i < expositions.length; i++) {
+      if (expositions[i].id == selectedExpoId) {
+        selectedExpo = expositions[i];
+        break;
+      }
+    }
+    console.log("Exposition trouvée :", selectedExpo);
+    if (selectedExpo) {
+      const dateDebut = selectedExpo.date_debut;
+      const dateFin = selectedExpo.date_fin;
+      document.getElementById("dateInput").min = dateDebut;
+      document.getElementById("dateInput").max = dateFin;
     }
   };
+  
 
-  const handleSaveQRCodeAsPDF = async (qrCodeDataURL) => {
-    try {
-      if (expositions.length === 0) {
-        console.error('Aucune donnée d exposition disponible.');
-        return;
-      }
-      const doc = new jsPDF();
-      const selectedExpo = expositions.find(expo => expo.id === formData.id_expo);
-      if (selectedExpo) {
-        const nomExposition = `Nom de l'exposition : ${selectedExpo.nom}\n`;
-        const dateDebut = `Date de début : ${selectedExpo.date_debut}\n`;
-        const dateFin = `Date de fin : ${selectedExpo.date_fin}\n`;
-        const lieu = `Lieu : ${selectedExpo.lieu}\n\n`;
-        const descriptionText = nomExposition + dateDebut + dateFin + lieu;
-        doc.text(descriptionText, 10, 20);
-      }
-      const dateSelectionnee = `Date sélectionnée : ${dayjs(formData.date_debut).format('DD/MM/YYYY')}\n`;
-      const heureSelectionnee = `Heure sélectionnée : ${formData.heure}\n\n`;
-      doc.text(dateSelectionnee, 10, 70);
-      doc.text(heureSelectionnee, 10, 80);
-      const nomPrenom = `Nom : ${formData.nom}\nPrénom : ${formData.prenom}\n\n`;
-      doc.text(nomPrenom, 10, 100);
-      const textePresentation = "Veuillez vous présenter à l'entrée muni de votre QRCode";
-      doc.text(textePresentation, 10, 130);
-      doc.addImage(qrCodeDataURL, 'PNG', 10, 150, 50, 50);
-      doc.save('qrcode.pdf');
-      console.log('QR code sauvegardé en PDF');
-    } catch (error) {
-      console.error('Erreur lors de la sauvegarde du QR code en PDF:', error);
-    }
-  };
+  
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const convertirEnDate = (dateString) => {
-      const [jour, mois, annee] = dateString.split('/');
-      return new Date(annee, mois - 1, jour);
-    };
     try {
-      const selectedExpo = expositions.find(expo =>
-        convertirEnDate(expo.date_debut) <= convertirEnDate(formData.date_debut) &&
-        convertirEnDate(expo.date_fin) >= convertirEnDate(formData.date_debut) &&
-        formData.heure >= expo.heure_debut &&
-        formData.heure <= expo.heure_fin
-      );
-      if (!selectedExpo) {
-        alert('Aucune exposition correspondante trouvée pour la date et l\'heure sélectionnées.');
-        return;
-      }
-      await axios.post('/api/register_user', formData);
-      console.log('Données soumises avec succès.');
-      const qrCodeDataURL = await generateQRCode();
-      await handleSaveQRCodeAsPDF(qrCodeDataURL);
+      console.log(formData);
     } catch (error) {
       console.error('Erreur lors de la soumission du formulaire:', error);
     }
@@ -132,9 +80,9 @@ const FormEnregistrements = () => {
                 <p className='label'>Prénom</p>
                 <p className='label'>Nom</p>
                 <p className='label'>Mail</p>
+                <p className='label'>Expositions</p>
                 <p className='label'>Date d'entrée</p>
                 <p className='label'>Heure d'entrée</p>
-                <p className='label'>Expositions</p>
               </div>
               <div className='form-input'>
                 <div className='div-input'>
@@ -170,6 +118,14 @@ const FormEnregistrements = () => {
                     required
                   />
                 </div>
+                <select className='select-exposition' value={formData.id_expo} onChange={handleExpoChange}>
+                  <option value=""></option>
+                  {expositions.map((expo) => (
+                    <option key={expo.id} value={expo.id}>
+                      {expo.nom} - {expo.heure_debut.slice(0, -3)} à {expo.heure_fin.slice(0, -3)} - {expo.date_debut} à {expo.date_fin}
+                    </option>
+                  ))}
+                </select>
                 <div className='div-input'>
                   <input
                     className='date_fin'
@@ -177,28 +133,9 @@ const FormEnregistrements = () => {
                     placeholder="jj/mm/aaaa"
                     value={formData.date_debut}
                     onChange={handleChange}
+                    id="dateInput"
                   />
                 </div>
-                <div className='div-input'>
-                  <input
-                    className='heure'
-                    type="time"
-                    placeholder="21:30"
-                    name="heure"
-                    value={formData.heure}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <select className='select-exposition' value={formData.id_expo} onChange={handleExpoChange}>
-                  <option value=""></option>
-                  {expositions.map((expo, index) => (
-                    <option key={index} value={expo.id} disabled={new Date(formData.date_debut) < new Date(expo.date_debut) || new Date(formData.date_debut) > new Date(expo.date_fin) || (formData.heure < expo.heure_debut || formData.heure > expo.heure_fin)}>
-                      {expo.nom}
-                      {expo.nom} - {expo.heure_debut.slice(0, -3)} à {expo.heure_fin.slice(0, -3)} - {expo.date_debut} à {expo.date_fin}
-                    </option>
-                  ))}
-                </select>
               </div>
             </div>
             <center className='button-reserved-registeruser'>
