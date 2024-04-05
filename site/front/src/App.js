5cdb20e89cde736056bd10431cc2578c7ccf5dd3
bubu54 <bunelierpro@gmail.com>
2024-04-05 05:22:50 -0700

    up

--- site/front/src/App.js
+++ site/front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EnregistrementExpo from "./pages/register_expo.js";
 import ListesExpos from "./pages/liste_expo.js";
@@ -6,7 +6,6 @@ import HistoExpo from "./pages/graph.js";
 import FormEnregistrement from './pages/register_user.js'
 import Header from './pages/header.js'
 import Login from './pages/login.js'
-import ProtectedRoute from './pages/protectedroute.js';
 
 
 const App = () => {
@@ -17,6 +16,7 @@ const App = () => {
                 <Route path="/" element={<ListesExpos />} />
                 <Route path="/historique" element={<HistoExpo />} />
                 <Route path="/register_user" element={<FormEnregistrement />} />
+                <Route path="/register_expo" element={<EnregistrementExpo />} />
                 <Route path="/header" element={<Header />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
