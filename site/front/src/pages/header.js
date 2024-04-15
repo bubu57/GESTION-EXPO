91909d1cc7fdd6a33bf1e05c497713acd2c5e833
bubu57 <bunelierpro@gmail.com>
2024-04-15 16:15:46 +0200

    up

--- site/front/src/pages/header.js
+++ site/front/src/pages/header.js
@@ -17,10 +17,10 @@ const Header = () => {
         <div className='header-logo'><img className='header-img' src={logo} alt="Logo"></img> </div>
         <Button color="success" variant="text" onClick={toggleMenu}>Menu</Button>
         <div className="menu-items">
-          <Link to="/"><Button color="success" variant="text">Accueil</Button></Link>
-          <Link to="/historique"><Button color="success" variant="text">Historique</Button></Link>
-          <Link to="/register_user"><Button color="success" variant="text">Inscription</Button></Link>
-          <Link to="/login"><Button color="success" variant="contained">Admin</Button></Link>
+          <Link to="/"><Button className="button" color="success" variant="text">Accueil</Button></Link>
+          <Link to="/historique"><Button  className="button" color="success" variant="text">Historique</Button></Link>
+          <Link to="/register_user"><Button  className="button" color="success" variant="text">Inscription</Button></Link>
+          <Link to="/login"><Button  className="button" color="success" variant="contained">Admin</Button></Link>
         </div>
       </div>
     </header>
