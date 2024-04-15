4963b022a4a59f91230b993cddc2fe4a884f937c
bubu54 <bunelierpro@gmail.com>
2024-04-15 02:27:18 -0700

    up

--- site/front/src/pages/header.js
+++ site/front/src/pages/header.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../img/logo.svg';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import "../styles/header.css";
 
-const Header = () =>  {
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <header className='header-content'>
+    <header className={`header-content ${menuOpen ? 'menu-open' : ''}`}>
       <div className='header-label-group'>
         <div className='header-logo'><img className='header-img' src={logo} alt="Logo"></img> </div>
-        <Link to="/"><Button color="success" variant="text">Acceuil</Button></Link>
-        <Link to="/historique"><Button color="success" variant="text">Historique</Button></Link>
-        <Link to="/register_user"><Button color="success" variant="text">Inscripription</Button></Link>
-        <Link to="/login"><Button color="success" variant="contained">Admin</Button></Link>
+        <Button color="success" variant="text" onClick={toggleMenu}>Menu</Button>
+        <div className="menu-items">
+          <Link to="/"><Button color="success" variant="text">Accueil</Button></Link>
+          <Link to="/historique"><Button color="success" variant="text">Historique</Button></Link>
+          <Link to="/register_user"><Button color="success" variant="text">Inscription</Button></Link>
+          <Link to="/login"><Button color="success" variant="contained">Admin</Button></Link>
+        </div>
       </div>
     </header>
   );
 };
 
 export default Header;
+
