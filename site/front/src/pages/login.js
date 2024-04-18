3afdb09cf6237eda800cf4c9f95f4af190aa9cc8
bubu57 <bunelierpro@gmail.com>
2024-04-18 05:04:35 -0700

    up

--- site/front/src/pages/login.js
+++ site/front/src/pages/login.js
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Header from './header.js';
-import Footer from './footer.js';
 import Button from '@mui/material/Button';
 import "../styles/login.css";
-import "../styles/admin.css";
 
 const Login = () => {
   const [formData, setFormData] = useState({ username: '', password: '' });
@@ -96,17 +94,15 @@ const Login = () => {
     }
   };
 
-
-
   return (
     <div>
       <Header />
       {isLoginFormVisible && (
-        <div className='form'>
-          <center><p className='title'>Connexion</p></center>
+        <div className='login-form'>
+          <center><p className='login-title'>Connexion</p></center>
           <form onSubmit={handleSubmit}>
-            <div className='form-block'>
-              <div className='div-input'>
+            <div className='login-form-block'>
+              <div className='login-div-input'>
                 <input
                   type="text"
                   placeholder="Nom d'utilisateur"
@@ -116,7 +112,7 @@ const Login = () => {
                   required
                 />
               </div>
-              <div className='div-input'>
+              <div className='login-div-input'>
                 <input
                   type="password"
                   placeholder="Mot de passe"
@@ -126,10 +122,10 @@ const Login = () => {
                   required
                 />
               </div>
-              {error && <p className="error">{error}</p>}
+              {error && <p className="login-error">{error}</p>}
             </div>
             <center>
-              <div className='but'>
+              <div className='login-but'>
                 <Button color="success" variant="contained" type="submit" >Se connecter</Button>
               </div>
             </center>
@@ -138,37 +134,37 @@ const Login = () => {
       )}
 
       {isAdminVisible && (
-      <div className='admin-container'>
-      <h2>Liste des Admins</h2>
-      <ul>
-        {admins.map(admin => (
-          <li key={admin.id}>
-            <span>{admin.User}</span>
-            <Button variant="text" size="small" color="error" onClick={() => handleDeleteAdmin(admin.id)}>X</Button>
-          </li>
-        ))}
-      </ul>
-      <h2>Liste des Expositions</h2>
-      <ul>
-        {expositions.map(expo => (
-          <li key={expo.id}>
-            <span>{expo.nom}</span>
-            <Button variant="text" size="small" color="error" onClick={() => handleDeleteExpo(expo.id)}>X</Button>
-          </li>
-        ))}
-      </ul>
-      <div className='but'>
-        <Link to="/register_expo"><Button color="success" variant="contained">Ajouter une expo</Button></Link>
-      </div>
-      <h2>Ajouter un nouvel Admin</h2>
-      <div className='add-admin'>
-        <input type="text" name="username" placeholder="Nom d'utilisateur" value={newAdminData.username} onChange={handleeChange} />
-        <input type="password" name="password" placeholder="Mot de passe" value={newAdminData.password} onChange={handleeChange} />
-        <div className='but'>
-          <Button color="success" variant="contained" onClick={handleAddAdmin}>Ajouter</Button>
+        <div className='login-admin-container'>
+          <h2>Liste des Admins</h2>
+          <ul>
+            {admins.map(admin => (
+              <li key={admin.id}>
+                <span>{admin.User}</span>
+                <Button variant="text" size="small" color="error" onClick={() => handleDeleteAdmin(admin.id)}>X</Button>
+              </li>
+            ))}
+          </ul>
+          <h2>Liste des Expositions</h2>
+          <ul>
+            {expositions.map(expo => (
+              <li key={expo.id}>
+                <span>{expo.nom}</span>
+                <Button variant="text" size="small" color="error" onClick={() => handleDeleteExpo(expo.id)}>X</Button>
+              </li>
+            ))}
+          </ul>
+          <div className='login-but'>
+            <Link to="/register_expo"><Button color="success" variant="contained">Ajouter une expo</Button></Link>
+          </div>
+          <h2>Ajouter un nouvel Admin</h2>
+          <div className='login-add-admin'>
+            <input type="text" name="username" placeholder="Nom d'utilisateur" value={newAdminData.username} onChange={handleeChange} />
+            <input type="password" name="password" placeholder="Mot de passe" value={newAdminData.password} onChange={handleeChange} />
+            <div className='login-but'>
+              <Button color="success" variant="contained" onClick={handleAddAdmin}>Ajouter</Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
       )}
     </div>
   );
