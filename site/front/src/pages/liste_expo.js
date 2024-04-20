2984948958d61709e14d58b7246ebdd98c845cdd
bubu57 <bunelierpro@gmail.com>
2024-04-20 08:52:42 +0200

    up

--- site/front/src/pages/liste_expo.js
+++ site/front/src/pages/liste_expo.js
@@ -181,10 +181,10 @@ const ListesExpos = () => {
                       <div className='acceuil-maps-box'><a className='acceuil-maps-link' href={"https://www.google.fr/maps/place/" + expo.latitude + "," + expo.longitude}>Voir sur maps</a></div>
                     </div>
                   )}
-                  <div className='acceuil-boutton-box'>
-                    <Button color="success" variant="contained" onClick={() => handleVoirPlusClick(expo)}>Details</Button>
-                  </div>
                 </div>
+              </div>
+              <div className='acceuil-boutton-box'>
+                    <Button color="success" variant="contained" onClick={() => handleVoirPlusClick(expo)}>Details</Button>
               </div>
             </div>
           ))}
