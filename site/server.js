0ce046780ac35ccbd17021c162c303428421766b
bubu57 <bunelierpro@gmail.com>
2024-04-22 07:34:08 +0200

    up

--- site/server.js
+++ site/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');  // Ajout du module mysql2
-require('dotenv').config()
-const app = express();
+const mysql = require('mysql2');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
-// recuperation du port via .env sinon utilise le port 5000
-const PORT = process.env.PORT;
+const app = express();
+const PORT = process.env.PORT || 5000;
 const SECRET_KEY = 'secretkey123';
 
 const pool = mysql.createPool({
-  connectionLimit: 10, // Limite de connexions simultanées
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -26,7 +25,7 @@ const queryAsync = (query) => {
         return reject(err);
       }
       connection.query(query, (error, results) => {
-        connection.release(); // Libérer la connexion après l'exécution de la requête
+        connection.release();
         if (error) {
           reject(error);
         } else {
@@ -37,36 +36,17 @@ const queryAsync = (query) => {
   });
 };
 
-
-
-const enddb = async (connection) => {
-  console.log('Arrêt du serveur, fermeture de la connexion à la base de données...');
-  connection.end((err) => {
-    if (err) {
-      console.error('Erreur lors de la fermeture de la connexion à la base de données :', err);
-    } else {
-      console.log('Connexion à la base de données fermée avec succès');
-    }
-    process.exit(0); // Arrêtez le processus Node.js
-  });
-}
-
-
-
-app.use(express.json())
-app.use(express.static('front/build'))
+app.use(express.json());
+app.use(express.static('front/build'));
 app.use(bodyParser.json());
 
-app.post('/api/login', (req, res) => {
-  let connection
-  const { username, password } = req.body;
-  const sql = 'SELECT * FROM Admin WHERE User = ? AND Password = ?';
-  connection.query(sql, [username, password], (err, results) => {
-    if (err) {
-      console.error('Erreur lors de la recherche de l\'utilisateur dans la base de données :', err);
-      res.status(500).json({ error: 'Erreur interne du serveur' });
-      return;
-    }
+let quotanb = []
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const sql = 'SELECT * FROM Admin WHERE User = ? AND Password = ?';
+    const results = await queryAsync(sql, [username, password]);
     if (results.length > 0) {
       const user = results[0];
       const token = jwt.sign({ username: user.username, id: user.id }, SECRET_KEY);
@@ -74,8 +54,10 @@ app.post('/api/login', (req, res) => {
     } else {
       res.status(401).json({ error: 'Nom d\'utilisateur ou mot de passe incorrect' });
     }
-  });
-  connection.end();
+  } catch (error) {
+    console.error('Erreur lors de la recherche de l\'utilisateur dans la base de données :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
 });
 
 app.get('/api/app', async (req, res) => {
@@ -83,16 +65,14 @@ app.get('/api/app', async (req, res) => {
     const today = new Date().toISOString().split('T')[0];
     const expositionQuery = `SELECT *, DATE_FORMAT(date_debut, '%d/%m/%Y') AS date_debut, DATE_FORMAT(date_fin, '%d/%m/%Y') AS date_fin FROM Exposition WHERE date_fin >= '${today}'`;
     const lieuQuery = 'SELECT * FROM Lieu';
-
-    const expositionResults = await queryAsync(expositionQuery);
-    const lieuResults = await queryAsync(lieuQuery);
-
-    // Combine les données exposition et lieu
+    const [expositionResults, lieuResults] = await Promise.all([
+      queryAsync(expositionQuery),
+      queryAsync(lieuQuery)
+    ]);
     const combinedResults = expositionResults.map(exposition => {
       const lieu = lieuResults.find(l => l.id === exposition.id);
       return { ...exposition, ville: lieu.ville, numero: lieu.numero, rue: lieu.rue, cp: lieu.code_postal, latitude: lieu.latitude, longitude: lieu.longitude };
     });
-
     res.json(combinedResults);
   } catch (error) {
     console.error('Erreur:', error);
@@ -100,228 +80,114 @@ app.get('/api/app', async (req, res) => {
   }
 });
 
-
-
-let quotanb = [];
-
-app.post('/api/quota', (req, res) => {
-  // let connection
-  console.log(req.body);
-  const expositionQuery = `SELECT * FROM Visiteur WHERE id_expo = ${req.body.id_expo} AND date_entree = '${req.body.date_debut}';`;
-
-  connection.query(expositionQuery, (expositionErr, expositionResults) => {
-    if (expositionErr) {
-      console.error('Erreur lors de la récupération des données de la table Visiteur:', expositionErr);
-      res.status(500).json({ error: 'Erreur interne du serveur' });
-    } else {
-      quotanb = expositionResults;
-      res.json({ success: true, message: 'ok' });
-    }
-  });
-  connection.end();
-})
+app.post('/api/quota', async (req, res) => {
+  try {
+    console.log(req.body);
+    const expositionQuery = `SELECT * FROM Visiteur WHERE id_expo = ${req.body.id_expo} AND date_entree = '${req.body.date_debut}';`;
+    const expositionResults = await queryAsync(expositionQuery);
+    res.json({ success: true, message: 'ok' });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données de la table Visiteur:', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
+});
 
 app.get('/api/quotanb', (req, res) => {
   res.json({ quotanb });
 });
 
-
-
-app.post('/api/enregistrement', (req, res) => {
-  // let connection
-
-  const lieuQuery = `
-  INSERT INTO Lieu (numero, rue, code_postal, ville, latitude, longitude)
-  VALUES ("${req.body.numero}", "${req.body.rue}", "${req.body.code_postal}", "${req.body.ville}", "${req.body.latitude}", "${req.body.longitude}");
-  `;
-
-  const expoQuery = `
-    INSERT INTO Exposition (quota ,nom, type, date_debut, date_fin, heure_debut, heure_fin, estimation, visiteur)
-    VALUES (${req.body.quota} ,"${req.body.nom}", "${req.body.type}", "${req.body.date_debut}", "${req.body.date_fin}", "${req.body.heure_debut}", "${req.body.heure_fin}", "${req.body.estimation}", 0);
-  `;
-
-  // Commencez la transaction
-  connection.beginTransaction((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Erreur lors de la création d\'une nouvelle expo' });
-      return;
-    }
-
-    // Exécutez la première requête (lieuQuery)
-    connection.query(lieuQuery, (err, results) => {
-      if (err) {
-        console.error(err);
-        return connection.rollback(() => {
-          res.status(500).json({ error: 'Erreur lors de la création d\'une nouvelle expo' });
-        });
-      }
-
-      // Exécutez la deuxième requête (expoQuery)
-      connection.query(expoQuery, (err, results) => {
-        if (err) {
-          console.error(err);
-          return connection.rollback(() => {
-            res.status(500).json({ error: 'Erreur lors de la création d\'une nouvelle expo' });
-          });
-        }
-
-        // Commit si tout s'est bien passé
-        connection.commit((err) => {
-          if (err) {
-            console.error(err);
-            return connection.rollback(() => {
-              res.status(500).json({ error: 'Erreur lors de la création d\'une nouvelle expo' });
-            });
-          }
-
-          // Transaction réussie, envoyez la réponse au client
-          res.json({ success: true, message: 'Enregistrement réussi' });
-        });
-      });
-    });
-  });
-  connection.end();
+app.post('/api/enregistrement', async (req, res) => {
+  try {
+    const lieuQuery = `INSERT INTO Lieu (numero, rue, code_postal, ville, latitude, longitude) VALUES ("${req.body.numero}", "${req.body.rue}", "${req.body.code_postal}", "${req.body.ville}", "${req.body.latitude}", "${req.body.longitude}")`;
+    const expoQuery = `INSERT INTO Exposition (quota ,nom, type, date_debut, date_fin, heure_debut, heure_fin, estimation, visiteur) VALUES (${req.body.quota}, "${req.body.nom}", "${req.body.type}", "${req.body.date_debut}", "${req.body.date_fin}", "${req.body.heure_debut}", "${req.body.heure_fin}", "${req.body.estimation}", 0)`;
+    await queryAsync(lieuQuery);
+    await queryAsync(expoQuery);
+    res.json({ success: true, message: 'Enregistrement réussi' });
+  } catch (error) {
+    console.error('Erreur lors de la création d\'une nouvelle expo :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
 });
 
-
-
-app.post('/api/register_user', (req, res) => {
-  // let connection
-
-  const lieuQuery = `
-  INSERT INTO Visiteur (nom, prenom, email, id_expo, date_entree, heure)
-  VALUES ("${req.body.nom}", "${req.body.prenom}", "${req.body.mail}", "${req.body.id_expo}", "${req.body.date_debut}", "${req.body.heure}");
-  `;
-
-  // Commencez la transaction
-  connection.beginTransaction((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Erreur lors de la création d\'un nouvelle user' });
-      return;
-    }
-
-  // Exécutez la requête 
-  connection.query(lieuQuery, (err, results) => {
-    if (err) {
-      console.error(err);
-      return connection.rollback(() => {
-        res.status(500).json({ error: 'Erreur lors de la création d\'un nouvelle user' });
-      });
-    }
-
-      // Commit si tout s'est bien passé
-      connection.commit((err) => {
-        if (err) {
-          console.error(err);
-          return connection.rollback(() => {
-            res.status(500).json({ error: 'Erreur lors de la création d\'un nouvelle user' });
-          });
-        }
-        res.json({ success: true, message: 'Enregistrement réussi' });
-      });
-    });
-  });
-  connection.end();
+app.post('/api/register_user', async (req, res) => {
+  try {
+    const lieuQuery = `INSERT INTO Visiteur (nom, prenom, email, id_expo, date_entree, heure) VALUES ("${req.body.nom}", "${req.body.prenom}", "${req.body.mail}", "${req.body.id_expo}", "${req.body.date_debut}", "${req.body.heure}")`;
+    await queryAsync(lieuQuery);
+    res.json({ success: true, message: 'Enregistrement réussi' });
+  } catch (error) {
+    console.error('Erreur lors de la création d\'un nouvelle user :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
 });
 
-
-
-
-
-app.get('/api/admins', (req, res) => {
-  // let connection
-  const query = 'SELECT * FROM Admin';
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Erreur lors de la récupération des administrateurs :', error);
-      res.status(500).json({ error: 'Erreur lors de la récupération des administrateurs' });
-      return;
-    }
+app.get('/api/admins', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM Admin';
+    const results = await queryAsync(query);
     res.json(results);
-  });
-  connection.end();
+  } catch (error) {
+    console.error('Erreur lors de la récupération des administrateurs :', error);
+    res.status(500).json({ error: 'Erreur lors de la récupération des administrateurs' });
+  }
 });
 
-app.post('/api/admins', (req, res) => {
-  // let connection
-  const query = `INSERT INTO Admin (User, Password) VALUES ('${req.body.username}', '${req.body.password}')`;
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Erreur lors de la creation de l\'administrateur :', error);
-      res.status(500).json({ error: 'Erreur lors de la creation de l\'administrateur :' });
-      return;
-    }
+app.post('/api/admins', async (req, res) => {
+  try {
+    const query = `INSERT INTO Admin (User, Password) VALUES ('${req.body.username}', '${req.body.password}')`;
+    await queryAsync(query);
     res.json({ success: true, message: 'Enregistrement réussi' });
-  });
-  connection.end();
+  } catch (error) {
+    console.error('Erreur lors de la création de l\'administrateur :', error);
+    res.status(500).json({ error: 'Erreur lors de la création de l\'administrateur :' });
+  }
 });
 
-app.post('/api/dadmins', (req, res) => {
-  // let connection
-  const query = `DELETE FROM Admin WHERE id = ${req.body.id}`;
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Erreur lors de la suppression de l\'administrateur :', error);
-      res.status(500).json({ error: 'Erreur lors de la suppression de l\'administrateur :' });
-      return;
-    }
+app.post('/api/dadmins', async (req, res) => {
+  try {
+    const query = `DELETE FROM Admin WHERE id = ${req.body.id}`;
+    await queryAsync(query);
     res.json({ success: true, message: 'Enregistrement réussi' });
-  });
-  connection.end();
+  } catch (error) {
+    console.error('Erreur lors de la suppression de l\'administrateur :', error);
+    res.status(500).json({ error: 'Erreur lors de la suppression de l\'administrateur :' });
+  }
 });
 
-app.post('/api/dexpo', (req, res) => {
-  // let connection
-  const query = `DELETE Exposition, Lieu FROM Exposition INNER JOIN Lieu ON Exposition.id = Lieu.id WHERE Exposition.id = ${req.body.id}`;
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Erreur lors de la suppression de l\'administrateur :', error);
-      res.status(500).json({ error: 'Erreur lors de la suppression de l\'administrateur :' });
-      return;
-    }
+app.post('/api/dexpo', async (req, res) => {
+  try {
+    const query = `DELETE Exposition, Lieu FROM Exposition INNER JOIN Lieu ON Exposition.id = Lieu.id WHERE Exposition.id = ${req.body.id}`;
+    await queryAsync(query);
     res.json({ success: true, message: 'Enregistrement réussi' });
-  });
-  connection.end();
+  } catch (error) {
+    console.error('Erreur lors de la suppression de l\'administrateur :', error);
+    res.status(500).json({ error: 'Erreur lors de la suppression de l\'administrateur :' });
+  }
 });
 
-
-
-app.get('/api/map', (req, res) => {
-  // let connection
-  const today = new Date().toISOString().split('T')[0];
-  const expositionQuery = `SELECT *, DATE_FORMAT(date_debut, '%d/%m/%Y') AS date_debut, DATE_FORMAT(date_fin, '%d/%m/%Y') AS date_fin FROM Exposition WHERE date_fin <= '${today}'`;
-  const lieuQuery = 'SELECT * FROM Lieu';
-
-  connection.query(expositionQuery, (expositionErr, expositionResults) => {
-    if (expositionErr) {
-      console.error('Erreur lors de la récupération des données de la table exposition:', expositionErr);
-      res.status(500).json({ error: 'Erreur interne du serveur' });
-    } else {
-      connection.query(lieuQuery, (lieuErr, lieuResults) => {
-        if (lieuErr) {
-          console.error('Erreur lors de la récupération des données de la table lieu:', lieuErr);
-          res.status(500).json({ error: 'Erreur interne du serveur' });
-        } else {
-          // Combine les données exposition et lieu
-          const combinedResults = expositionResults.map(exposition => {
-            const lieu = lieuResults.find(l => l.id === exposition.id);
-            return { ...exposition, ville: lieu.ville, numero: lieu.numero, rue: lieu.rue, cp: lieu.code_postale, latitude: lieu.latitude, longitude: lieu.longitude };
-          });
-
-          res.json(combinedResults);
-        }
-      });
-    }
-  });
-  connection.end();
+app.get('/api/map', async (req, res) => {
+  try {
+    const today = new Date().toISOString().split('T')[0];
+    const expositionQuery = `SELECT *, DATE_FORMAT(date_debut, '%d/%m/%Y') AS date_debut, DATE_FORMAT(date_fin, '%d/%m/%Y') AS date_fin FROM Exposition WHERE date_fin <= '${today}'`;
+    const lieuQuery = 'SELECT * FROM Lieu';
+    const [expositionResults, lieuResults] = await Promise.all([
+      queryAsync(expositionQuery),
+      queryAsync(lieuQuery)
+    ]);
+    const combinedResults = expositionResults.map(exposition => {
+      const lieu = lieuResults.find(l => l.id === exposition.id);
+      return { ...exposition, ville: lieu.ville, numero: lieu.numero, rue: lieu.rue, cp: lieu.code_postale, latitude: lieu.latitude, longitude: lieu.longitude };
+    });
+    res.json(combinedResults);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données pour la carte:', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
 });
 
 app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, '/front/build/index.html'));
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`server lancé sur le port: ${PORT}`);
-})
+  console.log(`Serveur lancé sur le port: ${PORT}`);
+});
