ec96a26b970ce3e3443978decfcdbe521d2eafec
bunelierpro <bunelierpro@gmail.com>
2024-05-12 23:52:59 +0000

    up

--- site/server.js
+++ site/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: 'config/.env' });
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
