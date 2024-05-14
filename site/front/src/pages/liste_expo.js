c000a8b8cf50ee58b4327a628be3a32568c42515
bunelierpro <bunelierpro@gmail.com>
2024-05-14 08:08:51 +0000

    up

--- site/front/src/pages/liste_expo.js
+++ site/front/src/pages/liste_expo.js
@@ -68,6 +68,7 @@ const ListesExpos = () => {
       // Filtrer les expositions en fonction de la date
       const expositionsFiltrees = expositions.filter(expo => {
         const dateFiltreLowerCase = dateFiltre.toLowerCase();
+        console.log(dateFiltreLowerCase);
         const dateDebutLowerCase = expo.date_debut.toLowerCase();
         const dateFinLowerCase = expo.date_fin.toLowerCase();
         
